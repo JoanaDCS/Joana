222ad5b0b94eb3a140ee31d3f4b7fb1205bfa62f
JoanaDCS <joana.darc.ufpi@gmail.com>
2015-04-07 19:56:29 -0300

    Adicionando o Bean de Requisição para cadastro de usuários

---
+++ src/main/java/controller/UsuarioBean.java
@@ -0,0 +1,33 @@
+package controller;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+import DAO.DAO;
+import Model.Usuario;
+
+@ManagedBean(name="user")
+@RequestScoped
+public class UsuarioBean {
+
+	private Usuario user = new Usuario();
+	
+	public void adiciona(){
+		DAO<Usuario> dao = new DAO<Usuario>(Usuario.class);
+		dao.adiciona(user);
+		this.user = new Usuario();
+	}
+
+	public Usuario getUser() {
+		return user;
+	}
+
+	public void setUser(Usuario user) {
+		this.user = user;
+	}
+	
+	
+	
+	
+	
+}
