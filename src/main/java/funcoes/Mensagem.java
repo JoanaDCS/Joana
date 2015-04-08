3e801b670d218b2a00e2b4f032faeeb827dd002d
JoanaDCS <joana.darc.ufpi@gmail.com>
2015-04-07 23:55:52 -0300

    CLasse de mensagem

---
+++ src/main/java/funcoes/Mensagem.java
@@ -0,0 +1,15 @@
+package funcoes;
+
+public class Mensagem {
+	private String email;
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+
+}
