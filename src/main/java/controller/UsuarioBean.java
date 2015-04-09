19e85b61997159dc47d5c612dcc40c2bad7c8598
JoanaDCS <joana.darc.ufpi@gmail.com>
2015-04-09 00:39:39 -0300

    Construção da tela para visualizar usuários cadastrados.

--- src/main/java/controller/UsuarioBean.java
+++ src/main/java/controller/UsuarioBean.java
@@ -1,5 +1,7 @@
 package controller;
 
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 
@@ -11,12 +13,26 @@ import Model.Usuario;
 public class UsuarioBean {
 
 	private Usuario user = new Usuario();
+	private List<Usuario> usuarios;
 	
+	
+	public List<Usuario> getUsuarios() {
+		if(usuarios == null){
+			usuarios = new DAO<Usuario>(Usuario.class).listaTodos();
+		}
+		return usuarios;
+	}
+
+	public void setUsuarios(List<Usuario> usuarios) {
+		this.usuarios = usuarios;
+	}
+
 	public void adiciona(){
 		DAO<Usuario> dao = new DAO<Usuario>(Usuario.class);
 		dao.adiciona(user);
 		this.user = new Usuario();
 	}
+	
 
 	public Usuario getUser() {
 		return user;
